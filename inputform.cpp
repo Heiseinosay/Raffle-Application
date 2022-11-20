6b93bfdb37811b8857921662dadc08dc0553940c
Heiseinosay <underbumpbump@gmail.com>
2022-11-21 02:34:20 +0800

    Add Files

---
+++ inputform.cpp
@@ -0,0 +1,58 @@
+#include "inputform.h"
+#include "ui_inputform.h"
+#include <QMessageBox>
+InputForm::InputForm(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::InputForm)
+{
+    ui->setupUi(this);
+    //DropDown
+    ui->PrizeSelectionMode->addItem("Random");
+    ui->PrizeSelectionMode->addItem("Descending");
+    ui->PrizeSelectionMode->addItem("Ascending");
+
+    //Icon
+    ui->NameDelete->setIcon(QIcon(":/img/img/trash.png"));
+    ui->NameEdit->setIcon(QIcon(":/img/img/edit.png"));
+
+    ui->PrizeDelete->setIcon(QIcon(":/img/img/trash.png"));
+    ui->PrizeEdit->setIcon(QIcon(":/img/img/edit.png"));
+
+    ui->btnInfromation->setIcon(QIcon(":/img/img/about.png"));
+    ui->btnSave->setIcon(QIcon(":/img/img/diskette.png"));
+    ui->btnOpen->setIcon(QIcon(":/img/img/folder.png"));
+
+    //Name Line Edit
+    ui->LineEditName1->setDisabled(true);
+
+
+
+}
+
+InputForm::~InputForm()
+{
+    delete ui;
+}
+
+void InputForm::on_btnStart_clicked()
+{
+    hide();
+    OutputPresentation outputPresentation;
+    outputPresentation.setModal(true);
+    outputPresentation.exec();
+}
+
+
+void InputForm::on_btnQuit_clicked()
+{
+    QMessageBox::StandardButton answer = QMessageBox::question(
+                this, "Quit", "Recent changes may not have been saved. Are you sure you want to quit?",
+                QMessageBox::Yes | QMessageBox::No);
+
+    if (answer == QMessageBox::Yes) {
+        QApplication::quit();
+    }
+
+
+}
+
---
+++ outputpresentation.cpp
@@ -0,0 +1,31 @@
+#include "outputpresentation.h"
+#include "ui_outputpresentation.h"
+
+#include "inputform.h"
+
+OutputPresentation::OutputPresentation(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OutputPresentation)
+{
+    ui->setupUi(this);
+    //Z-order
+    ui->btnEdit->raise();
+
+    //Icons
+    ui->btnDeleteName->setIcon(QIcon(":/img/img/trash.png"));
+    ui->btnEdit->setIcon(QIcon(":/img/img/edit.png"));
+}
+
+OutputPresentation::~OutputPresentation()
+{
+    delete ui;
+}
+
+void OutputPresentation::on_btnEdit_clicked()
+{
+    hide();
+    InputForm inputForm;
+    inputForm.setModal(true);
+    inputForm.exec();
+}
+
