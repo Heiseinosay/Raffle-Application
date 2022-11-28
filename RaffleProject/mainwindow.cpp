1667c715d0dc749aa277e1b6ac341bbd1f73a25b
Heiseinosay <84405551+Heiseinosay@users.noreply.github.com>
2022-11-29 01:14:27 +0800

    Updated UI

--- RaffleProject/mainwindow.cpp
+++ RaffleProject/mainwindow.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-#include "dbmanager.h"
 #include "ui_mainwindow.h"
 
 #include <QPixmap>
@@ -9,23 +8,11 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-
     ui->setupUi(this);
     QPixmap pix(":/img/img/Ticket.png");
     ui->LabelTicket->setPixmap(pix);
-    ui->LabelTicket->setPixmap(pix.scaled(140, 140,Qt::KeepAspectRatio));
+    ui->LabelTicket->setPixmap(pix.scaled(50, 37,Qt::KeepAspectRatio));
 
-    QPixmap background(":/img/img/confetti-png.png");
-    ui->LandingBackground->setPixmap(background);
-    ui->LandingBackground->setPixmap(background.scaled(1300, 600,Qt::KeepAspectRatio));
-
-    QPainter painter(this);
-    painter.setOpacity(0.2);
-    // use scaled image or if needed not scaled m_avatar
-    painter.drawPixmap(0, 0, background);
-
-    //Z-order
-    ui->LandingBackground->lower();
 }
 
 MainWindow::~MainWindow()
